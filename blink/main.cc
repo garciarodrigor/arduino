5115684ec42886a259f26159902393adf2ca7da7
Rodrigo Garcia <rodrigo@rgnu.com.ar>
2013-08-14 22:14:21 -0300

    Define DELAY constant

--- blink/main.cc
+++ blink/main.cc
@@ -9,24 +9,22 @@
 #include <Arduino.h>
 
 #define LED_PIN 13
+#define DELAY 500
+
 
 void setup()
 {
   // initialize the digital pin as an output.
   // Pin 13 has an LED connected on most Arduino boards:
   pinMode(LED_PIN, OUTPUT);
-  
-  Serial.begin(9600);
 }
 
 void loop()
 {
   digitalWrite(LED_PIN, HIGH);   // set the LED on
-  delay(100);              // wait for a second
+  delay(DELAY);              // wait for a second
   
   digitalWrite(LED_PIN, LOW);    // set the LED off
-  delay(1000);              // wait for a second
-  
-  Serial.println(millis());
+  delay(DELAY);              // wait for a second
 }
 
