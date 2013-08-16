55c67e95766ef79b0bfdca4e6de7ec2c12ccad11
Rodrigo Garcia <rodrigo@rgnu.com.ar>
2013-08-16 02:14:44 -0300

    Add patterns 3 (draw lines)

--- drawbot/pattern/main.cc
+++ drawbot/pattern/main.cc
@@ -21,22 +21,27 @@
 /*
  * Stepper 1 Config
  */
-#define SP1_1 2
-#define SP1_2 3
-#define SP1_3 4
-#define SP1_4 5
+#define SP1_1 6
+#define SP1_2 7
+#define SP1_3 8
+#define SP1_4 9
 #define SP1_STEPS 48
 #define SP1_MAXSPD 60
+#define SP1_DH -1
+#define SP1_DL  1
 
 /*
  * Stepper 2 Config
  */
-#define SP2_1 6
-#define SP2_2 7
-#define SP2_3 8
-#define SP2_4 9
+#define SP2_1 2
+#define SP2_2 3
+#define SP2_3 4
+#define SP2_4 5
 #define SP2_STEPS 48
 #define SP2_MAXSPD 60
+#define SP2_DH  1
+#define SP2_DL -1
+
 
 void stepperInit(Stepper &sp, int speed)
 {
@@ -59,16 +64,16 @@ void draw_pattern_1(Stepper &sp1, Stepper &sp2, int sz1, int sz2, int step, int 
     while (loop > 0) {
 
         int size  = sz1;
-    
+
         while (size < sz2) {
-            sp1.step(size);    
+            sp1.step(size);
             size += step;
             delay(10);
-                
-            sp2.step(size);    
+
+            sp2.step(size);
             size += step;
             delay(10);
-            
+
             sp1.step(-size);
             size += step;
             delay(10);
@@ -76,10 +81,8 @@ void draw_pattern_1(Stepper &sp1, Stepper &sp2, int sz1, int sz2, int step, int 
             sp2.step(-size);
             size += step;
             delay(10);
-            
-            //step++;
         }
-        
+
         loop--; 
     }
 }
@@ -97,10 +100,10 @@ void draw_pattern_2(Stepper &sp1, Stepper &sp2, int size, int step, int loop)
 {
     while (loop > 0 || step == 0) {
 
-        sp1.step(size);    
+        sp1.step(size);
         delay(10);
             
-        sp2.step(step);    
+        sp2.step(step);
         delay(10);
         
         sp1.step(-size);
@@ -110,9 +113,30 @@ void draw_pattern_2(Stepper &sp1, Stepper &sp2, int size, int step, int loop)
         delay(10);
 
         step--;
-        loop--; 
+        loop--;
     }
 }
+
+/**
+ * Draw a pattern
+ *
+ * sp1: Stepper 1
+ * sp2: Stepper 2
+ * size: Pattern Size
+ * dir: Direction
+ */
+void draw_pattern_3(Stepper &sp1, Stepper &sp2, int size, int d1, int d2)
+{
+    while (size) {
+
+        sp1.step(d1);
+        sp2.step(d2);
+        delay(25);
+
+        size --;
+    }
+}
+
 
 void setup()
 {
@@ -122,12 +146,26 @@ void setup()
     stepperInit(s1, SP1_MAXSPD);
     stepperInit(s2, SP2_MAXSPD);
 
-    draw_pattern_2(s2, s1, -48, 0, 12);
-    draw_pattern_1(s2, s1, 2, 12, 1, 4);
-    draw_pattern_2(s1, s2, 48, 0, 12); 
+
+    /**
+     * Home walls
+     */
+    draw_pattern_3(s1, s2, 48,  SP1_DL, SP2_DH);
+    draw_pattern_3(s1, s2, 48,  SP1_DL, SP2_DL);
+    draw_pattern_3(s1, s2, 48,  SP1_DH, SP2_DL);
+    draw_pattern_3(s1, s2, 48,  SP1_DH, SP2_DH);
+    
+    draw_pattern_3(s1, s2, 48,  SP1_DL, SP2_DL);
+
+    /**
+     * Home roof
+     */
+    draw_pattern_3(s1, s2, 48,  SP1_DL, 0);
+
+    draw_pattern_3(s1, s2, 48,  0, SP2_DH);
+
 }
 
 void loop()
 {
 }
-
